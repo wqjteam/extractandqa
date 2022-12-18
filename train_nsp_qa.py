5e1776630e3af015352b5af9d7a68fc8f005da84
githubwqj <893453172@qq.com>
2022-12-18 23:04:47 +0800

    tijiao

--- train_nsp_qa.py
+++ train_nsp_qa.py
@@ -1,1 +1,12 @@
 ﻿# -*- coding: utf-8 -*-
+from dataclasses import dataclass
+from typing import Any, Optional, Tuple, Mapping, List, Union, Dict
+
+import numpy as np
+from transformers import PreTrainedTokenizerBase
+from transformers.data.data_collator import _numpy_collate_batch, _torch_collate_batch, _tf_collate_batch, \
+    DataCollatorMixin
+
+
+
+
