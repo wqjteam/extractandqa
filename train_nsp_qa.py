82863df618cdf1983245103aaf11f760240bed62
githubwqj2 <2812930196@qq.com>
2022-12-20 18:05:00 +0800

    更改以修改好数据源

--- train_nsp_qa.py
+++ train_nsp_qa.py
@@ -3,10 +3,20 @@ from dataclasses import dataclass
 from typing import Any, Optional, Tuple, Mapping, List, Union, Dict
 
 import numpy as np
-from transformers import PreTrainedTokenizerBase
+import transformers
+from transformers import PreTrainedTokenizerBase, AutoTokenizer, AutoModel
 from transformers.data.data_collator import _numpy_collate_batch, _torch_collate_batch, _tf_collate_batch, \
     DataCollatorMixin
 
+
+# DataCollatorForLanguageModelingSpecial
+model_name = 'hfl/chinese-xlnet-base'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModel.from_pretrained(model_name)
+data_collator = transformers.DataCollatorForLanguageModeling(tokenizer=tokenizer,
+                                                             mlm=True,
+                                                             mlm_probability=0.15,
+                                                             return_tensors="pt")
 
 
 
