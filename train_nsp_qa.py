0209da691813cb6c2030b32fb410965e43effca3
githubwqj <893453172@qq.com>
2022-12-24 23:57:35 +0800

    tijiao

--- train_nsp_qa.py
+++ train_nsp_qa.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from typing import Any, Optional, Tuple, Mapping, List, Union, Dict
 
 import numpy as np
+import pandas as pd
 import torch
 import torch.utils.data as Data
 import transformers
@@ -22,14 +23,19 @@ model = AutoModel.from_pretrained(model_name)
 #                                                              return_tensors="pt")
 
 data_collator = DataCollatorForLanguageModelingSpecial(tokenizer=tokenizer,
-                                                mlm=True,
-                                                mlm_probability=0.15,
-                                                return_tensors="pt")
+                                                       mlm=True,
+                                                       mlm_probability=0.15,
+                                                       return_tensors="pt")
+
+'''
+获取数据
+'''
+passage_keyword_json = pd.read_json("../data/origin/intercontest/passage_qa_keyword.json", orient='records',
+                                    lines=True).head(100).drop("spos", axis=1)
+passage_keyword_json = passage_keyword_json.explode("q_a")
 sent = "我爱北京天安门，天安门上太阳升"
 question = "我爱什么"
 # 创建一个实例，参数是tokenizer
-
-
 encoded_dict = tokenizer.encode_plus(
     sent,  # 输入文本
     question,  #
@@ -46,12 +52,24 @@ input_ids = [torch.tensor(encoded_dict['input_ids'])]
 # -100的位置的词没有被mask，
 output = data_collator(input_ids)
 print(output)
-# train_dataset = Data.TensorDataset(inputing, target)
-# train_dataset = (inputing, target)
-# train_dataloader = Data.DataLoader(
-#     input_ids, shuffle=True, collate_fn=data_collator, batch_size=10
-# )
 
 
-# for returndata in train_dataloader:
-#     print(returndata)
+def create_batch(data,tokenizer,data_collator):
+    encoded_dict = tokenizer.encode_plus(
+        sent,  # 输入文本
+        question,  #
+        add_special_tokens=True,  # 添加 '[CLS]' 和 '[SEP]'
+        max_length=32,  # 填充 & 截断长度
+        truncation=True,
+        pad_to_max_length=True,
+        return_attention_mask=True,  # 返回 attn. masks.
+    )
+    return ('')
+
+
+train_dataloader = Data.DataLoader(
+    input_ids, shuffle=True, collate_fn=data_collator, batch_size=10
+)
+
+for returndata in train_dataloader:
+    print(returndata)
