0d6f311f8db02244cc4c6584ff4d0fde087495eb
githubwqj2 <2812930196@qq.com>
2023-02-01 18:10:11 +0800

    更改以修改好数据源

--- test_nsp_qa.py
+++ test_nsp_qa.py
@@ -4,4 +4,4 @@ from PraticeOfTransformers.CustomModel import BertForUnionNspAndQA
 
 model = BertForUnionNspAndQA()
 model.load_state_dict(torch.load("model/path1"))
-model.eval()
+model.eval() #==  self.train(False) ，使用评估模式（model.train()训练模式） 在评估模式下，batchNorm层，dropout层等用于优化训练而添加的网络层会被关闭，从而使得评估时不会发生偏移。
