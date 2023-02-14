52d392fad7439af0e19365440d59c68b44442009
githubwqj <893453172@qq.com>
2023-02-14 22:47:06 +0800

    添加

--- train_nsp_qa.py
+++ train_nsp_qa.py
@@ -382,7 +382,7 @@ for epoch in range(epoch_size):  # 所有数据迭代总的次数
 
     # 每5个epoch保存一次
     if (epoch + 1) % 5 == 0:
-        torch.save(model.state_dict(), 'save_model/nsp_qa/epoch_%d' % (epoch + 1))
+        torch.save(model.state_dict(), 'save_model/nsp_qa/nsp_qa_epoch_%d' % (epoch + 1))
 
 # 最后保存一下
-torch.save(model.state_dict(), 'save_model/nsp_qa/epoch_%d' % (epoch_size))
+torch.save(model.state_dict(), 'save_model/nsp_qa/nsp_qa_epoch_%d' % (epoch_size))
