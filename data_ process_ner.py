9a9c5e498a9a6eaf68fe7bb8a9552ff34ac7d52f
githubwqj2 <2812930196@qq.com>
2023-02-24 14:27:01 +0800

    添加ner的处理

--- data_ process_ner.py
+++ data_ process_ner.py
@@ -7,16 +7,51 @@ import pandas
 import pandas as pd
 import numpy as np
 
+from sqlalchemy import create_engine
+from sqlalchemy.dialects.mysql import pymysql
 
-qa_json = pd.read_json("data/origin/intercontest/passage_qa_keyword.json", orient='records', lines=True)
-a=qa_json[qa_json.apply(lambda x: len(x['keyword'])==0, axis=1)]
-# def applykeyword():
+qa_json = pd.read_json(path_or_buf='data/origin/intercontest/passage_qa_keyword.json', orient='records', lines=True)
 
-
-a=qa_json[qa_json.apply(lambda x: len(x['keyword'])==0, axis=1)]
+a=qa_json
 
 pd.set_option('display.max_rows', None)
+# print(a)
+
+# conn = pymysql.Connect(
+#   host = '192.168.4.144',
+#   port = 3306,
+#   user = 'root',
+#   passwd = '123456',
+#   db = 'relic_data',
+#   charset = 'utf8'
+#   )
+
+
+engine = create_engine("mysql+pymysql://root:123456@192.168.4.144:3306/relic_data?charset=utf8")
+
+# a.to_sql(name='user_tableq', con=engine, if_exists='replace', index=False)
+
+#读取
+a_keyword_json = pd.read_sql_table(table_name='user_tableq', con=engine).to_numpy()
+
+def getkeyword(inputdata):
+    if len(inputdata.keyword)>0:
+        return inputdata.keyword
+    else:
+        for ssnupmy in a_keyword_json:
+            if ssnupmy[0]==inputdata.sentence:
+                return ssnupmy[1].split('|')
+a['keyword']=a.apply(getkeyword, axis=1)
+
+a=qa_json[qa_json.apply(lambda x: len(x['keyword'])>1, axis=1)]
 print(a)
+#a.to_json('data/origin/intercontest/passage_qa_keyword.json', force_ascii=False,orient='records', lines=True)
+# a_json.to_json('data/origin/intercontest/qa_keyword.json', force_ascii=False,orient='records', lines=True)
+
+# with engine.begin() as conn:
+#     pass
+# 数据已经组织好，无需再次插入
+# qa_json.to_sql(name='qa', con=conn, if_exists='replace', index=False)
 # ner_json_withkeyword = qa_json.loc[:, ["sentence",'keyword']]
 # ner_json_withkeyword.to_csv('data/origin/intercontest/passage_ner_withkeyword.txt',index=False,header=0)
 #
