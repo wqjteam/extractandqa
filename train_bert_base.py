086b2bfadc009913aefc746b26a2e617a8bd31c7
githubwqj2 <2812930196@qq.com>
2023-03-01 16:27:32 +0800

    z

--- train_bert_base.py
+++ train_bert_base.py
@@ -67,10 +67,12 @@ union_pd = pd.concat(
     [passage_keyword_json_realrelic.loc[:, ['sentence', 'keyword']], passage_keyword_json_virtualrelic], axis=0)
 train_data = union_pd.values
 
+# train_data = train_data[:20]
+
 _, dev_data = Data.random_split(union_pd.values, [int(len(union_pd.values) * 0.9),
                                                   len(union_pd.values) - int(
                                                       len(union_pd.values) * 0.9)])
-
+# dev_data=dev_data[:2]
 
 def create_batch(data, tokenizer, data_collator, keyword_flag=False):
     text, keyword = zip(*data)  # arrat的四列 转为tuple
@@ -99,7 +101,8 @@ def create_batch(data, tokenizer, data_collator, keyword_flag=False):
             encoded_dict_keywords.append([])
         else:
             encoded_dict_keyword = tokenizer.batch_encode_plus(batch_text_or_text_pairs=ks,
-                                                               add_special_tokens=False,  # 添加 '[CLS]' 和 '[SEP]'
+                                                               add_special_tokens=True,
+                                                               # 添加 '[CLS]' 和 '[SEP]' 需要添加，即使是不需要用到nsp的内容
                                                                pad_to_max_length=False,
                                                                return_attention_mask=False
                                                                )
@@ -132,6 +135,14 @@ dev_dataloader = Data.DataLoader(
     dev_data, shuffle=True, collate_fn=create_batch_partial, batch_size=batch_size
 )
 
+# 实例化相关metrics的计算对象   len(ner_id_label)+1是为了ignore_index用 他不允许为负数值 这里忽略了，所以不影响结果
+model_recall = torchmetrics.Recall(num_classes=model.config.vocab_size + 1, mdmc_average='global',
+                                   ignore_index=model.config.vocab_size)
+model_precision = torchmetrics.Precision(num_classes=model.config.vocab_size + 1, mdmc_average='global',
+                                         ignore_index=model.config.vocab_size)
+model_f1 = torchmetrics.F1Score(num_classes=model.config.vocab_size + 1, mdmc_average='global',
+                                ignore_index=model.config.vocab_size)
+
 # 看是否用cpu或者gpu训练
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("-----------------------------------训练模式为%s------------------------------------" % device)
@@ -145,10 +156,14 @@ model.to(device)
 
 viz = Visdom(env=u'bert_base_special_train_without_nsp')  # 可视化
 name = ['mlm_loss', 'total_loss']
+name_precision_recall_f1 = ['precision_score', 'recall_score', 'f1_score']
 viz.line(Y=[(0., 0.)], X=[(0., 0.)], win="pitcure_1",
          opts=dict(title='train_loss', legend=name, xlabel='epoch', ylabel='loss', markers=False))  # 绘制起始位置 #win指的是图形id
 viz.line(Y=[(0., 0.)], X=[(0., 0.)], win="pitcure_2",
          opts=dict(title='eval_loss', legend=name, xlabel='epoch', ylabel='loss', markers=False))  # 绘制起始位置
+viz.line(Y=[(0., 0., 0.)], X=[(0., 0., 0.)], win="pitcure_3",
+         opts=dict(title='eval_precision_recall_f1', legend=name_precision_recall_f1, xlabel='epoch', ylabel='score',
+                   markers=False))  # 绘制起始位置
 
 
 #  评估函数，用作训练一轮，评估一轮使用
@@ -183,12 +198,20 @@ def evaluate(model, eval_data_loader, epoch):
         masked_lm_loss = loss_fct(prediction_logits.view(-1, model_config.vocab_size),
                                   mask_input_labels.view(-1))
 
-        '''
-        nsp loss 计算
-        '''
-        # next_sentence_loss = loss_fct(seq_relationship_logits.view(-1, 2), nsp_labels.view(-1))
-
         total_loss = masked_lm_loss
+
+        predict = torch.argmax(prediction_logits, dim=2)
+        mask_input_labels[mask_input_labels == -100] = model_config.vocab_size
+
+        '''
+        update 是计算当个batch的值  compute计算所有累加的值
+        '''
+        precision_score = model_precision(predict, mask_input_labels)
+        model_precision.update(predict, mask_input_labels)
+        recall_score = model_recall(predict, mask_input_labels)
+        model_recall.update(predict, mask_input_labels)
+        f1_score = model_f1(predict, mask_input_labels)
+        model_f1.update(predict, mask_input_labels)
 
         '''
         实际预测值与目标值的
@@ -204,13 +227,17 @@ def evaluate(model, eval_data_loader, epoch):
         eval_mlm_loss += masked_lm_loss.detach()
         # eval_nsp_loss += next_sentence_loss.detach()
         eval_total_loss += total_loss.detach()
-        print('--eval---eopch: %d----mlm_loss: %f------ 损失函数: %.6f' % (
-            epoch, masked_lm_loss, total_loss))
+        print('--eval---eopch: %d----precision_score: %f----recall_score: %f----f1_score: %f----mlm_loss: %f------ 损失函数: %.6f-' % (
+            epoch, precision_score, recall_score, f1_score, masked_lm_loss, total_loss))
 
     viz.line(Y=[
         (eval_mlm_loss / eval_step, eval_total_loss / eval_step)], X=[(epoch + 1, epoch + 1)], win="pitcure_2",
         update='append')
-
+    viz.line(Y=[(model_precision.compute().to('cpu'), model_recall.compute().to('cpu'), model_f1.compute().to('cpu'))],
+             X=[(epoch + 1, epoch + 1, epoch + 1)], win="pitcure_3", update='append')
+    model_precision.reset()
+    model_recall.reset()
+    model_f1.reset()
 
 # 进行训练
 for epoch in range(epoch_size):  # 所有数据迭代总的次数
